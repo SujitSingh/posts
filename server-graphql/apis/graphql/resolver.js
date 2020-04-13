d1d4f3004ec113ec24c0aa9ba4c803f1099d713d
Sujit Kumar Singh <sujit98322@gmail.com>
2020-04-13 18:33:51 +0530

    Adding validator for graphql API

--- server-graphql/apis/graphql/resolver.js
+++ server-graphql/apis/graphql/resolver.js
@@ -1,9 +1,21 @@
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const User = require('../models/user');
 
 module.exports = {
   createUser: async ({ userInput }, req) => {
+    const errors = [];
+    if (!validator.isEmail(userInput.email) || !validator) {
+      errors.push({ message: 'Invalid email' });
+    }
+    if (validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, { min: 5 })) {
+      errors.push({ message: 'Password field should be min 5 characters long' });
+    }
+    if (errors.length) {
+      throw new Error(JSON.stringify(errors)); // send validation errors
+    }
+
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
       throw new Error('User already exists');
--- server-graphql/apis/graphql/schema.js
+++ server-graphql/apis/graphql/schema.js
@@ -28,6 +28,10 @@ module.exports = buildSchema(`
     createUser(userInput: UserInputData): User!
   }
 
+  type RootQuery {
+    hello: String!
+  }
+
   schema {
     query: RootQuery
     mutation: RootMutation
