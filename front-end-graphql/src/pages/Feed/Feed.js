9867a9f872508503cc36f5667358ef56d9a80cea
Sujit Kumar Singh <sujit98322@gmail.com>
2020-05-26 00:29:12 +0530

    COrrection for pagination when thir item gets added.

--- front-end-graphql/src/pages/Feed/Feed.js
+++ front-end-graphql/src/pages/Feed/Feed.js
@@ -278,20 +278,25 @@ class Feed extends Component {
         // add/edit posts to state
         this.setState(prevState => {
           let updatedPosts = [...prevState.posts];
+          let updatedTotalPost = prevState.totalPosts;
           if (prevState.editPost) {
             const postIndex = prevState.posts.findIndex(
               p => p.id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
           } else {
-            updatedPosts.pop();
+            updatedTotalPost++;
+            if (prevState.posts.length >= 2) {
+              updatedPosts.pop();
+            }
             updatedPosts.unshift(post);
           }
           return {
             posts: updatedPosts,
             isEditing: false,
             editPost: null,
-            editLoading: false
+            editLoading: false,
+            totalPosts: updatedTotalPost
           };
         });
       })
